aa76688029ef1c284a95c24a5178369bc5dc82bc
Alameh777 <alamehmhmd5@gmail.com>
2025-03-30 22:20:01 +0300

    ra7 ente7er

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import signup from './signup';
+
 
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<signup />} />
+      <Route path="/" element={<Home/>} />
+      
     </Routes>
   </BrowserRouter>
   );
